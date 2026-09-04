86f98dccbb6309355aefa1f66334bd368e56939b
darenas-RL <darenas@ucol.mx>
2026-09-04 09:16:52 -0600

    hola

--- main.c
+++ main.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 int main(void) {
-    printf("=========================");
-    printf("¡HOLA, GITHUB!");
     printf("=========================\n");
-    printf("Mi Nombre es: Andre de Jesus");
-    printf("Estoy aprendiendo programacion");
-    printf("Este es mi primer programa");
+    printf("¡HOLA, GITHUB!\n");
+    printf("=========================\n");
+    printf("Mi Nombre es: Andre de Jesus\n");
+    printf("Estoy aprendiendo programacion\n");
+    printf("Este es mi primer programa\n");
     printf("Publicado en github\n");
     printf("¡Bienvenido a mi repositorio!");
     return 0;
