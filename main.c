64fb6ef4bb01722ccc0e7ca2e4721459d01690e8
darenas-RL <darenas@ucol.mx>
2026-09-04 09:10:54 -0600

    Initial commit

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void) {
+    printf("=========================");
+    printf("¡HOLA, GITHUB!");
+    printf("=========================\n");
+    printf("Mi Nombre es: Andre de Jesus");
+    printf("Estoy aprendiendo programacion");
+    printf("Este es mi primer programa");
+    printf("Publicado en github\n");
+    printf("¡Bienvenido a mi repositorio!");
+    return 0;
+}
